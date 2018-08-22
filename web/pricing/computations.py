f86160a435dbcf054ec1945ce8b128305edbc214
Aaruran Chandrasekhar <aaruranc@umich.edu>
2018-08-22 14:48:49 -0400

    debugging Black-Scholes Pricing

--- web/pricing/computations.py
+++ web/pricing/computations.py
@@ -1,175 +1,188 @@
 from pathlib import Path
 import os
 import pandas as pd
-from math import sqrt, log, e, ceil
+from math import sqrt, log, exp, ceil
 from community import strategies
 from scipy.stats import norm
 
 
-# def str_adjust(date):
-#     length = len(date)
-#     if length == 6:
-#         new_date = "0" + date[0:2] + "0" + date[2:]
-#         return  new_date
-#     if length == 7:
-#         if date[1] == "/":
-#             new_date = "0" + date
-#             return new_date
-#         else:
-#             new_date = date[0:3] + "0" + date[3:]
-#             return new_date
-#     if length == 8:
-#         return date
+def handle_strategy(query, query_file):
 
-def option_payoff(a, b, d, df, title, index):
-    n = len(df)
+    strategy = query['trading_strategy']
+    df = pd.read_csv(query_file)
 
+    if strategy == 'Calls':
+        return
+    elif strategy == 'Puts':
+        return
+    elif strategy == 'Straddles':
+        strategies.straddle(query, query_file)
+        return
 
-
+    ##### Add the rest of the strategies
 
     return
 
+def option_price(option_type, volatility_type, parameters):
 
-def option_price(a, d1, d2, parameters):
+    # if volatility_type == 'VNM':
+    #     d1 = parameters['d1_no_mean']
+    #     d2 = parameters['d2_no_mean']
+    # elif volatility_type == 'VM':
+    #     d1 = parameters['d1_mean']
+    #     d2 = parameters['d2_mean']
+
+    d1 = parameters['d1']
+    d2 = parameters['d2']
     m = parameters['spot']
     n = parameters['strike']
     o = parameters['rf']
     p = parameters['length']
 
-    if a == 'Calls':
-        x = (norm.cdf(d1) * m) - (norm.cdf(d2) * n * (e ** (-1 * o * p)))
-    elif a == 'Puts':
-        x = (norm.cdf(-1 * d2) * n * (e ** (-1 * o * p))) - (norm.cdf(-1 * d1) * m)
+    if option_type == 'Calls':
+        x = (norm.cdf(d1) * m) - (norm.cdf(d2) * n * (exp(-o * p)))
+    elif option_type == 'Puts':
+        x = (norm.cdf(-d2) * n * exp(-1 * o * p)) - (norm.cdf(-d1) * m)
+
     return x
 
 
-
-def BS_option_calcs(df, parameters, title, index):
+def BS_option_calcs(parameters):
 
     options = ['Calls', 'Puts']
-    volatilities = ['VM', 'VNM']
-    type = ['Price', 'Payoff', 'ROI']
+    # volatilities = ['VM', 'VNM']
 
     d = {}
+    # for a in options:
+    #     e = {}
+    #     for b in volatilities:
+    #         e.update({b: option_price(a, b, parameters)})
+    #     d.update({a: e})
+    # d.update({'input': parameters})
+
+
     for a in options:
-        for b in volatilities:
-            if b == 'VM':
-                d1 = parameters['d1_mean']
-                d2 = parameters['d2_mean']
-            else:
-                d1 = parameters['d1_no_mean']
-                d2 = parameters['d2_no_mean']
-            for c in type:
-                if c == 'Price':
-                    x = option_price(a, d1, d2, parameters)
-                elif c == 'Payoff':
-                    x = option_payoff(a, b, d, df, title, index)
-                else:
-                    x = d[a][b]['Payoff'] / d[a][b]['Price']
+        d.update({a: option_price(a, 0, parameters)})
+
+    return d
 
 
-                d.update({a: {b : {c: x}}})
+def compute_d1(v, l, r, rf):
+    # print('Vol')
+    # print(v)
+    # print('Length')
+    # print(l)
+    # print('Ratio')
+    # print(r)
+    # print('Risk Free')
+    # print(rf)
+    x = (log(r) + ((rf + ((v ** 2) / 2)) * l)) / (v * sqrt(l))
+    # print('d1')
+    # print(x)
+    return x
 
 
-    return {}
+def compute_d2(d1, v, l):
+    y = d1 - (v * sqrt(l))
+    # print('d2')
+    # print(y)
+    # print('__________')
+    return y
 
 
 def BS_dict(df, index, length, percentage):
 
-    d = {'length': length}
-
-    if (length + 3) > index:
-        return d
-
-    spot = df['Price'][index]
-    ratio = percentage / 100
-    strike = spot * ratio
-    d.update({'strike': strike, 'spot': spot})
-
+    year_length = length / 365
     title = option_label(length)
     v_m = title + '-VM'
-    v_nm = title + '-VNM'
+    # v_nm = title + '-VNM'
     rf_title = title + '-LIBOR'
-    risk_free = df[rf_title][index]
-    d.update({'rf': risk_free})
-    vol_mean = df[v_m][index]
-    vol_no_mean = df[v_nm][index]
 
-    compute_d1 = lambda v, l, r, rf : (1/(v*sqrt(l)))*(log((1/r))+(rf+((v**2)/2)*l))
-    compute_d2 = lambda d1, v, l: d1-(v*sqrt(l))
+    ratio = 1 / percentage
+    spot = df['Price'][index]
+    strike = spot * percentage
+    rf = df[rf_title][index]
 
-    d1_mean = compute_d1(vol_mean, length, ratio, risk_free)
-    d2_mean = compute_d2(d1_mean, vol_mean, length)
-    d1_no_mean = compute_d1(vol_no_mean, length, ratio, risk_free)
-    d2_no_mean = compute_d2(d1_no_mean, vol_no_mean, length)
+    # vol_mean = float(df[v_m][index])
+    # vol_no_mean = float(df[v_nm][index])
 
-    d.update({'d1_mean': d1_mean, 'd2_mean': d2_mean, 'd1_no_mean': d1_no_mean, 'd2_no_mean': d2_no_mean})
+    vol = float(df[v_m][index])
+    d1 = compute_d1(vol, year_length, ratio, rf)
+    d2 = compute_d2(d1, vol, year_length)
+
+    d = {'index': index, 'length': year_length, 'strike': strike, 'spot': spot, 'ratio': ratio,
+         'rf': rf, 'vol': vol, 'd1': d1, 'd2': d2}
+
+    # call = (norm.cdf(d1) * spot) - (norm.cdf(d2) * strike * (e ** (-1 * rf * year_length)))
+    # put = (norm.cdf(-1 * d2) * strike * (e ** (-1 * rf * year_length))) - (norm.cdf(-1 * d1) * spot)
+    # d.update({'call': call, 'put': put})
+
+    calcs = BS_option_calcs(d)
+    d.update({'calc_call': calcs['Calls'], 'calc_put': calcs['Puts']})
+    print(d)
+
+
+    # print('__________')
+    # print('Index:')
+    # print(index)
+    # print('__________')
+    # print('Spot')
+    # print(spot)
+    # print('Strike')
+    # print(strike)
+
+    # d1_mean = compute_d1(vol_mean, year_length, ratio, risk_free)
+    # d2_mean = compute_d2(d1_mean, vol_mean, year_length)
+    # d1_no_mean = compute_d1(vol_no_mean, year_length, ratio, risk_free)
+    # d2_no_mean = compute_d2(d1_no_mean, vol_no_mean, year_length)
+    #
+    # d.update({'d1_mean': d1_mean, 'd2_mean': d2_mean, 'd1_no_mean': d1_no_mean, 'd2_no_mean': d2_no_mean})
     return d
 
 
-def new_strike_data(query_file, df, length, strike):
+def new_strike_data(query, query_file, df):
+
+    length = int(query['option_length'])
+    strike = int(query['strike'])
 
     title = str(strike)
     c = title + '-Calls'
     p = title + '-Puts'
 
-    percentage = strike / 100
-    strike_df = pd.DataFrame(columns=['Price']).copy()
-    strike_df.rename(columns={'Price': title})
-    strike_df.applymap(lambda x: x * percentage)
 
-    headers = [(c + '-VM'), (c + '-VM-P'), (c + '-VM-ROI'), (c + '-VNM'), (c + '-VNM-P'), (c + '-VNM-ROI'),
-               (p + '-VM'), (p + '-VM-P'), (p + '-VM-ROI'), (p + '-VNM'), (p + '-VNM-P'), (p + '-VNM-ROI')]
+    ### Need a Datafram Column for Strike Prices
+
+    percentage = strike / 100
+    # headers = [(c + '-VM'), (c + '-VNM'), (p + '-VM'), (p + '-VNM')]
+    headers = [c, p]
     options_df = pd.DataFrame(columns=headers)
     d = {}
     size = len(df)
 
     for index, series in df.iterrows():
-        if size < (index + length + 1):
-            break
+        if length + 1 > index or size < (index + length):
+            # d.update({(c + '-VM'): '',
+            #           (c + '-VNM'): '',
+            #           (p + '-VM'): '',
+            #           (p + '-VNM'): ''
+            #           })
+            d.update({c: '', p: ''})
+        else:
+            parameters = BS_dict(df, index, length, percentage)
+            data = BS_option_calcs(parameters)
+            d.update({c: data['Calls'], p: data['Puts']})
 
-        parameters = BS_dict(df, index, length, percentage)
-        data = BS_option_calcs(df, parameters, title, index)
+            # d.update({(c + '-VM'): data['Calls']['VM'],
+            #           (c + '-VNM'): data['Calls']['VNM'],
+            #           (p + '-VM'): data['Puts']['VM'],
+            #           (p + '-VNM'): data['Puts']['VNM']
+            #           })
 
-        d.update({(c + '-VM'): data['Calls']['Mean']['Price'],
-                  (c + '-VM-P'): data['Calls']['Mean']['Payoff'],
-                  (c + '-VM-ROI'): data['Calls']['Mean']['ROI'],
-                  (c + '-VNM'): data['Calls']['No-Mean']['Price'],
-                  (c + '-VNM-P'): data['Calls']['No-Mean']['Payoff'],
-                  (c + '-VNM-ROI'): data['Calls']['Mean']['ROI'],
-                  (p + '-VM'): data['Puts']['Mean']['Price'],
-                  (p + '-VM-P'): data['Puts']['Mean']['Payoff'],
-                  (p + '-VM-ROI'): data['Puts']['Mean']['ROI'],
-                  (p + '-VNM'): data['Puts']['No-Mean']['Price'],
-                  (p + '-VNM-P'): data['Puts']['No-Mean']['Payoff'],
-                  (p + '-VNM-ROI'): data['Puts']['Mean']['ROI'],
-                  })
-        options_df.append(d, ignore_index=True)
+        options_df = options_df.append(d, ignore_index=True)
 
-    updated_df = pd.concat([df, strike_df, options_df], axis=1)
+    updated_df = pd.concat([df, options_df], axis=1)
+    print(updated_df)
     updated_df.to_csv(query_file)
-    return
-
-
-def handle_strategy(current_directory, current_file, df, length, strike, strategy):
-
-    method = str(strike) + '-' + strategy
-
-    if strategy == 'Calls':
-        strategies.calls()
-        return
-    elif strategy == 'Puts':
-        strategies.puts()
-        return
-    elif strategy == 'Straddles':
-        strategies.straddle(current_file, df, method)
-        return
-    elif strategy == 'Bear-Spreads':
-        strategies.bear_spreads(current_directory, length, current_file, df, strike, strategy, method)
-        return
-
-    ##### Add the rest of the strategies
-
     return
 
 
@@ -187,7 +200,7 @@ def volatility_mean(df, length, index):
     for j in range((index - length), index):
         diff = (log(df['Price'][j] / df['Price'][j-1]) - mu) ** 2
         total = total + diff
-    vol = 16 * (sqrt(total / (length-1)))
+    vol = 16 * (sqrt(total / (length-1))) * 100
     return vol
 
 
@@ -205,7 +218,7 @@ def volatility_no_mean(df, length, index):
     for j in range((index - length), index):
         diff = (log(df['Price'][j] / df['Price'][j - 1])) ** 2
         total = total + diff
-    vol = (sqrt(256 * (total / (length - 1))))
+    vol = (sqrt(256 * (total / (length - 1)))) * 100
     return vol
 
 
@@ -307,6 +320,7 @@ def grab_data(dates, length=0):
     data.reset_index(drop=True, inplace=True)
     return data
 
+
 def find_dates(df):
 
     n = len(df)
@@ -331,56 +345,115 @@ def option_label(length):
 
 def search_and_compute(query):
 
-    trading_strategy = query['trading_strategy']
     length = int(query['option_length'])
-    strike = query['strike']
     current_directory = Path(query['current_directory'])
     source = Path(query['source'])
+    strike = query['strike']
+    strategy = query['trading_strategy']
 
     option_length = option_label(length)
     loc = option_length + '.csv'
     query_file = current_directory / loc
-    print(query_file)
 
     if os.path.isfile(query_file):
-        # df = pd.read_csv(query_file)
-        # headers = list(df)
-        # strike_str = str(strike)
-        # if strike_str in headers:
-        #     method = strike_str + '-' + trading_strategy
-        #     if method in headers:
-        #         return
-        #     else:
-        #         handle_strategy(current_directory, query_file, df, length, strike, trading_strategy)
-        #         return
-        #
-        # else:
-        #     new_strike_data(query_file, df, length, strike)
-        #     handle_strategy(current_directory, query_file, df, length, strike, trading_strategy)
+        df = pd.read_csv(query_file)
+        headers = list(df)
+        test = strike + '-Calls'
+        if test in headers:
+            method = strike + '-' + strategy
+            if method in headers:
+                return
+            else:
+                handle_strategy(query, query_file)
+                return
+
+        else:
+            new_strike_data(query, query_file, df)
+            handle_strategy(query, query_file)
             return
 
     else:
         df = pd.read_csv(source)
+
         dates = find_dates(df)
         date_length = len(df['Datetime'])
+
         VIX = grab_data(dates)
         updated_VIX = fix_length(VIX, date_length)
+
         LIBOR = grab_data(dates, length=length)
         updated_LIBOR = fix_length(LIBOR, date_length, title=option_length)
+
         vols = calc_volatilities(df, length)
+
         df = pd.concat([df, updated_VIX, updated_LIBOR, vols], axis=1)
-        print(df)
 
-        # new_strike_data(query_file, df, length, strike)
-
-        df.to_csv(query_file, index=False)
+        new_strike_data(query, query_file, df)
+        handle_strategy(query, query_file)
 
         return
 
 if __name__ == '__main__':
     print('computations main executed')
 
-    test_query = {'trading_strategy': 'Calls', 'option_length': '3', 'strike': '90',
+    test_query = {'trading_strategy': 'Calls', 'option_length': '9', 'strike': '110',
                   'current_directory': 'data/1534716161', 'source': 'data/1534716161/AAPL.csv'}
 
-    search_and_compute(test_query)
+    # loc = Path(test_query['current_directory']) / '9-Day.csv'
+    # df = pd.read_csv(loc)
+    # new_strike_data(loc, df, test_query)
+
+    annual_rf = (1.121521739) ** (365 / 9)
+
+    x = {'i': 1390, 'l': 0.024657534246575342, 'st': 78.66573000000001, 'sp': 71.5143,
+         'r': 0.9090909090909091, 'rf': 1.121521739, 'v': 48.10019478494585, 'd1': 3.764296825178816,
+         'd2': -3.7887409643280927, 'c': 71.50239022171021, 'p': 78.41845694879935}
+
+    # d1 = 3.764296825178816
+    # d2 = -3.7887409643280927
+    # n_d1 = 0.9999164907998107
+    # n_d2 = 7.570634363292039e-05
+
+    calc_call = 71.50239022171021
+    calc_put = 8.41845694879935
+
+    web_call = .28 / .29
+    web_put = 7.3 / 7.4
+
+    # j = norm.ppf(.1118)
+    # k = -norm.ppf(.8882)
+    # l = norm.ppf((.0214 / (x['spot'] * sqrt(x['length']))) * exp(x['rf'] * x['length']))
+    # temp = (.0352 * x['spot'] * sqrt(x['length']) * x['vol'])
+    # print(temp)
+    # m = norm.ppf(temp)
+    # y = (j + k + l + m) / 4
+    # print('d1')
+    # print(j, k, l, m)
+    # print(y)
+    #
+    # a = norm.ppf((.0019 / (x['strike'] * x['length'])) * exp(x['rf'] * x['length']))
+    # b = -norm.ppf((.00175 / (x['strike'] * x['length'])) * exp(x['rf'] * x['length']))
+    # c = norm.ppf((.0214 / (x['strike'] * sqrt(x['length']))) * exp(x['rf'] * x['length']))
+    # d = norm.ppf(exp(x['rf'] * x['length']) * ((.0214 / (x['strike'] * sqrt(x['length']))) * exp(x['rf'] * x['length'])) / x['strike'])
+    # z = (a + b + c + d) / 4
+    # print('d2')
+    # print(a, b, c, d)
+    # print(z)
+
+    # d1 = -1.217011
+    # d2 = -3.095
+
+    delta = .1107
+    rho = .0019
+    d1 = norm.ppf(delta)
+    d2 = norm.ppf((rho / (x['st'] * x['l'])) * exp(x['rf'] * x['l']))
+
+
+    c = x['sp'] * norm.cdf(d1) - (x['st'] * exp(-1 * x['rf'] * x['l']) * norm.cdf(d2))
+    p = (x['st'] * exp(-1 * x['rf'] * x['l']) * norm.cdf(-d2)) - x['sp'] * norm.cdf(-d1)
+
+    print(c)
+    print(p)
+
+
+    # search_and_compute(test_query)

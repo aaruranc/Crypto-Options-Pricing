4b782b78c2f4a9c6deef0821ec27800a9a5aa71d
Aaruran Chandrasekhar <aaruranc@umich.edu>
2018-08-31 01:03:06 -0400

    one more push

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ def index():
 	    return render_template('index.html')
 
 	if request.method == 'POST':
-        start_time = str(int(time.time()))
-        session['start_time'] = start_time
+		start_time = str(int(time.time()))
+		session['start_time'] = start_time
             
 		if not request.form['asset'] or not request.form['start'] or not request.form['end']:
 			flags = ['Incomplete form data']
